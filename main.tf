2d32a4caec8e35346dc081fc4ce70764432183d3
Divya Khichi <divya_khichi@persistent.com>
2023-07-27 14:16:50 +0530

    Commit

--- main.tf
+++ main.tf
@@ -100,3 +100,74 @@ resource "dbt_cloud_job" "OperationSiteAgg_Test2" {
     }
 }
 
+# dbt_cloud_job.OperationSiteAgg_Test2:
+
+resource "dbt_cloud_job" "OperationSiteAgg_Test3" {
+
+    deferring_job_id     = 0
+
+    count                = length(var.environment_id)
+
+    environment_id       = var.environment_id[count.index]
+
+    execute_steps        = [
+
+        "dbt test",
+
+    ]
+
+    generate_docs        = false
+
+    is_active            = true
+
+    name                 = "OperationSiteAgg_Test3"
+
+    num_threads          = var.environment_id[count.index] == 218665 ? 6 : 4
+
+    project_id           = 269849
+
+    run_generate_sources = false
+
+   
+
+    schedule_days        = [
+
+        0,
+
+        1,
+
+        2,
+
+        3,
+
+        4,
+
+        5,
+
+        6,
+
+    ]
+
+    schedule_interval    = 22
+
+    schedule_type        = "days_of_week"
+
+    target_name          = "default"
+
+    timeout_seconds      = 0
+
+    triggers             = {
+
+        "custom_branch_only"   = true
+
+        "git_provider_webhook" = false
+
+        "github_webhook"       = false
+
+        "schedule"             = true
+
+    }
+
+}
+
+ 
